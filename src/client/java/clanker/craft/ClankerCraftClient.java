dda2484248629d16d85e4acd55bf156eb3c2192c
MaxW2002 <maxwillemsen171@gmail.com>
2025-10-07 17:24:09 +0200

    Initial commit

---
+++ src/client/java/clanker/craft/ClankerCraftClient.java
@@ -0,0 +1,10 @@
+package clanker.craft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClankerCraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
